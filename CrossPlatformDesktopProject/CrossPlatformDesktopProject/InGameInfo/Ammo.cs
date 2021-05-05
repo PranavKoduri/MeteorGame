c72136682efffe3270ff0826c68706c0cf3a7475
Pranav <pvkoduri@yahoo.com>
2021-05-05 01:31:38 -0400

    .

--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/InGameInfo/Ammo.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/InGameInfo/Ammo.cs
@@ -6,16 +6,18 @@ namespace CrossPlatformDesktopProject.InGameInfo
 {
     public class Ammo
     {
-        private const int maxAmmo = 10;
+        private int maxAmmo;
         private int currentAmmo;
 
         private const int ammoSpacing = 2;
         private const int verticalAmmoSpacing = 5;
         private readonly Vector2 topRightOffset = new Vector2(-10, 10);
 
-        private ISprite blueBorder; //readonly
-        private ISprite whiteBackground; //readonly
+        private ISprite blueBorder;
+        private ISprite whiteBackground;
         private List<ISprite> ammo;
+
+        private float windowWidth;
 
         private static Ammo ammoInstance = new Ammo();
         public static Ammo Instance
@@ -24,27 +26,36 @@ namespace CrossPlatformDesktopProject.InGameInfo
         }
         private Ammo()
         {
+            maxAmmo = 0;
             currentAmmo = maxAmmo;
         }
         public void Initialize(float windowWidth)
         {
-            Vector2 topLeft = new Vector2(windowWidth - (maxAmmo + 3) * ammoSpacing - maxAmmo * 4, 0) + topRightOffset;
-            blueBorder = SpriteFactory.Instance.BlueAmmoBorderSprite(topLeft, new Vector2((maxAmmo + 3) * ammoSpacing + maxAmmo * 4, verticalAmmoSpacing * 2 + 11 + ammoSpacing * 2));
-            whiteBackground = SpriteFactory.Instance.WhiteAmmoBackgroundSprite(topLeft + new Vector2(ammoSpacing, ammoSpacing), new Vector2((maxAmmo + 1) * ammoSpacing + maxAmmo * 4, verticalAmmoSpacing * 2 + 11));
-            ammo = new List<ISprite>();
-            for (int i = 0; i < maxAmmo; i++)
-            {
-                ammo.Add(SpriteFactory.Instance.BulletAmmoSprite(topLeft + new Vector2(2 * ammoSpacing + (4 + ammoSpacing) * i, ammoSpacing + verticalAmmoSpacing)));
-            }
+            this.windowWidth = windowWidth;
         }
-
         public void Draw()
         {
             blueBorder.Draw();
             whiteBackground.Draw();
             for (int i = 0; i < currentAmmo; i++) ammo[i].Draw();
         }
-        public int AmmoCount
+
+        public int MaxAmmo
+        {
+            set
+            {
+                if (value == maxAmmo) return;
+                Vector2 topLeft = new Vector2(windowWidth - (maxAmmo + 3) * ammoSpacing - maxAmmo * 4, 0) + topRightOffset;
+                blueBorder = SpriteFactory.Instance.BlueAmmoBorderSprite(topLeft, new Vector2((maxAmmo + 3) * ammoSpacing + maxAmmo * 4, verticalAmmoSpacing * 2 + 11 + ammoSpacing * 2));
+                whiteBackground = SpriteFactory.Instance.WhiteAmmoBackgroundSprite(topLeft + new Vector2(ammoSpacing, ammoSpacing), new Vector2((maxAmmo + 1) * ammoSpacing + maxAmmo * 4, verticalAmmoSpacing * 2 + 11));
+                ammo = new List<ISprite>();
+                for (int i = 0; i < maxAmmo; i++)
+                {
+                    ammo.Add(SpriteFactory.Instance.BulletAmmoSprite(topLeft + new Vector2(2 * ammoSpacing + (4 + ammoSpacing) * i, ammoSpacing + verticalAmmoSpacing)));
+                }
+            }
+        }
+        public int CurrentAmmo
         {
             set
             {
@@ -52,7 +63,6 @@ namespace CrossPlatformDesktopProject.InGameInfo
                 else if (value < 0) value = 0;
                 currentAmmo = value;
             }
-            get => currentAmmo;
         }
     }
 }
